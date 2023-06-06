4eade18e273e04be92ad5f5484a1cf7efbb91294
Sierra <m.sierra1037@hotmail.com>
2023-06-06 10:48:15 -0500

    planning of dasboard component

---
+++ src/Components/CountriesDashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CountriesDashboard = () => {
+
+    // fetch the countries 
+    // create react router 
+    // use intersection observer to show a reasonable amount of countries
+  return (
+    <div>CountriesDashboard</div>
+  )
+}
+
+export default CountriesDashboard
