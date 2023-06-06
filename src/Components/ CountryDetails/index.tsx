4269d25d44a34e57c1edf1666034d5b278107c36
Sierra <m.sierra1037@hotmail.com>
2023-06-06 10:54:54 -0500

    planning of the CountryDetails component

---
+++ src/Components/ CountryDetails/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CountryDetails = () => {
+
+
+    // get country by name or id (comes from arg)
+    // fetch it and show details 
+    /*
+        THE DETAILS SHOULD BE : 
+        - flag
+        - name
+        - native name
+        - population
+        - region 
+        - sub region
+        - capital 
+        - top level domain 
+        - currencies
+        - languages 
+        - border countries
+    */
+
+  return (
+    <div>CountryDetails</div>
+  )
+}
+
+export default CountryDetails
