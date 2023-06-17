694c64ab7e41bd6abb842b9ad3c29b5c4b52aa45
Sierra <m.sierra1037@hotmail.com>
2023-06-17 15:01:33 -0500

    feat: TopBar component added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './Styles/App.scss';
 import Router from './Components/Router';
 import CountriesDashboard from './Components/CountriesDashboard';
 import CountryDetails from './Components/ CountryDetails';
+import TopBar from './Components/TopBar';
 
 function App() {
 
   return (
     <div className="App">
-
+      <TopBar/>
       <Router path='/' Component={CountriesDashboard} ></Router>
       <Router path='/details' Component={CountryDetails} ></Router>
     </div>
---
+++ src/Components/TopBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopBar = () => {
+  return (
+    <header>
+        <h1>where in the word?</h1>
+    </header>
+  )
+}
+
+export default TopBar
