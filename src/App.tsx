bbe5f01c34105124e95d5668b0b1fa993265f8f3
Sierra <m.sierra1037@hotmail.com>
2023-06-17 15:17:21 -0500

    filtering bar component added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Router from './Components/Router';
 import CountriesDashboard from './Components/CountriesDashboard';
 import CountryDetails from './Components/ CountryDetails';
 import TopBar from './Components/TopBar';
+import FilteringBar from './Components/FilteringBar';
 
 function App() {
 
   return (
     <div className="App">
       <TopBar/>
+      <FilteringBar/>
       <Router path='/' Component={CountriesDashboard} ></Router>
       <Router path='/details' Component={CountryDetails} ></Router>
     </div>
--- src/Components/TopBar/index.tsx
+++ src/Components/TopBar/index.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const TopBar = () => {
   return (
-    <header>
+    <header className='app-header'>
         <h1>where in the word?</h1>
+        <button> {`(Icon)`}Dark Mode</button>
     </header>
   )
 }
