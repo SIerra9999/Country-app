d74e326d6fa1ef6ace39e9213c33899131295fed
Sierra <m.sierra1037@hotmail.com>
2023-06-21 14:02:59 -0500

    topbar component styles

--- src/Components/TopBar/index.tsx
+++ src/Components/TopBar/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const TopBar = () => {
   return (
-    <header className='app-header'>
-        <h1>where in the word?</h1>
-        <button> {`(Icon)`}Dark Mode</button>
+    <header className='top-bar'>
+        <h1 className='top-bar__welcome-message'>where in the word?</h1>
+        <button className='top-bar__theme-switcher'> {`(Icon)`}Dark Mode</button>
     </header>
   )
 }
