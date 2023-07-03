f6ab0f27341daa85dbda0c5c4c6a81a846907b52
Sierra <m.sierra1037@hotmail.com>
2023-07-02 22:20:45 -0500

    feat: theme switch button icon and color and background color transition

--- src/Components/TopBar/index.tsx
+++ src/Components/TopBar/index.tsx
@@ -5,18 +5,28 @@ import { useTheme } from '../ThemeContext'
 
 const TopBar = () => {
 
+
+
   const {theme, setTheme} = useTheme()
+
+  const switchThemeOnClick = ()=>{
+          
+    if(theme ==="light"){
+      setTheme("dark")
+    }else {
+      setTheme("light")
+    }
+  }
+
   return (
     <header className={`top-bar top-bar--${theme}`}>
         <h1 className='top-bar__welcome-message'>where in the word?</h1>
-        <button className='top-bar__theme-switcher' onClick={()=>{
-          
-          if(theme ==="light"){
-            setTheme("dark")
-          }else {
-            setTheme("light")
-          }
-        }}> {`(Icon)`}Dark Mode</button>
+        <button 
+        className='top-bar__theme-switcher' 
+        onClick={switchThemeOnClick}>
+          <span className="top-bar__theme-switcher__icon material-symbols-outlined">
+        brightness_low
+        </span></button>
     </header>
   )
 }
