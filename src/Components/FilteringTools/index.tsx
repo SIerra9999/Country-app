76d0541e7d16b729e29de81c4b9e0faeb5eafbd5
Sierra <m.sierra1037@hotmail.com>
2023-07-11 11:55:31 -0500

    chore: correctly adding the regions to the region dropdown

--- src/Components/FilteringTools/index.tsx
+++ src/Components/FilteringTools/index.tsx
@@ -6,12 +6,13 @@ const FilteringTools = () => {
 
       <input className="search-bar" type="text" />
 
-      <select name="sub-region" id="sub-region">
+      <select name="region-dropdown" id="sub-region">
 
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
         
       </select>
 
