a370176eaf60c2daeafe4a6d8a8e86a6fcd60233
Sierra <m.sierra1037@hotmail.com>
2023-07-11 12:58:44 -0500

    feat: searc bar component styles

--- src/Components/FilteringTools/index.tsx
+++ src/Components/FilteringTools/index.tsx
@@ -3,19 +3,18 @@ import React from "react";
 const FilteringTools = () => {
   return (
     <section className="filtering-tools">
-
-      <input className="search-bar" type="text" />
+      <div className="search-bar">
+        <span className="material-symbols-outlined search-bar__icon">search</span>
+        <input className="search-bar__input" placeholder="Search for a Country" type="text" />
+      </div>
 
       <select name="region-dropdown" id="sub-region">
-
         <option value="Africa">Africa</option>
         <option value="Americas">Americas</option>
         <option value="Asia">Asia</option>
         <option value="Europe">Europe</option>
         <option value="Oceania">Oceania</option>
-        
       </select>
-
     </section>
   );
 };
