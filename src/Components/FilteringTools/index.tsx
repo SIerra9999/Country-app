3f84cc662a39dd697a24da094bdcdc3938257c8c
Sierra <m.sierra1037@hotmail.com>
2023-07-12 11:58:02 -0500

    feat: dropdown styled with dark theme

--- src/Components/FilteringTools/index.tsx
+++ src/Components/FilteringTools/index.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { useTheme } from "../ThemeContext";
 const FilteringTools = () => {
-
-  const currentTheme = useTheme().theme
+  const currentTheme = useTheme().theme;
   return (
     <section className="filtering-tools">
       <div className={`search-bar search-bar--${currentTheme}`}>
-        <span className="material-symbols-outlined search-bar__icon">search</span>
-        <input className="search-bar__input" placeholder="Search for a country ..." type="text" />
+        <span className="material-symbols-outlined search-bar__icon">
+          search
+        </span>
+        <input
+          className="search-bar__input"
+          placeholder="Search for a country ..."
+          type="text"
+        />
       </div>
 
-      <select name="region-dropdown" id="sub-region">
-        <option value="Africa">Africa</option>
-        <option value="Americas">Americas</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceania">Oceania</option>
-      </select>
+      <div className={`region region--${currentTheme}`}>
+        <select
+          className="region__dropdown"
+          name="region-dropdown"
+          id="region-dropdown"
+          defaultValue={""}
+        >
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+
+        <span className="material-symbols-outlined dropdown-icon">
+          expand_more
+        </span>
+      </div>
     </section>
   );
 };
